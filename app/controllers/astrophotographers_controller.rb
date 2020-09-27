6e1c5bb94e8eb73296204d9e2a91b79644234fd9
chica25 <patricia@patriciaco.com>
2020-09-27 02:51:21 -0400

    Create Astrophotograher Controller actions and routes

--- app/controllers/astrophotographers_controller.rb
+++ app/controllers/astrophotographers_controller.rb
@@ -1,10 +1,42 @@
 class AstrophotographersController < ApplicationController
+    before_action :set_params, only: [:show, :edit, :update]
 
     def index 
-        @astrophotographer = Astrophotographer.all
+        @astrophotographers = Astrophotographer.all
     end
 
     def new
-       
+        @astrophotographer = Astrophotographer.new   
+    end
+
+    def show
+        set_params
+    end
+
+    def create
+        @astrophotographer = Astrophotographer.create(astro_params)
+        if @astrophotographer.save
+            redirect_to astrophotographer_path(@astrophotographer)
+        else
+            render :new
+        end
+    end
+
+    def edit 
+        set_params
+    end
+
+    def update
+        @astrophotographer.update(astro_params)
+        redirect_to astrophotographer_path(@astrophotographer)
+    end
+
+private
+    def astro_params
+        params.require(:astrophotographer).permmit(:username, :password_digest)
+    end
+
+    def set_params
+        @astrophotographer = Astrophotographer.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :stargazings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #resource :new
-get '/astrophotographers', to: 'astrophotographers#index'
+#get '/astrophotographers', to: 'astrophotographers#index'
 end
