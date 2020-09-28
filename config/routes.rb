6dfbdcec8b1dac4e59efded511dcb0dd46c69c79
chica25 <patricia@patriciaco.com>
2020-09-28 16:35:20 -0400

    Update Astrophotographers & Sessions controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'astrophotographers#new'
+  get '/login', to:, 'sessions#new'
   resources :astrophotographers
   resources :constellations
   resources :stargazings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-#resource :new
 #get '/astrophotographers', to: 'astrophotographers#index'
 end
