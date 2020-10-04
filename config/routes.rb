00960751ebfebd764be241309d927fb06d5c0cbc
chica25 <patricia@patriciaco.com>
2020-10-04 16:26:55 -0400

    Alter Sessions Routes for show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   post '/signup', to: 'astrophotographers#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get 'profile', to: 'session#show'
   delete '/logout', to: 'sessions#destroy'
-  resources :astrophotographers
-  resources :constellations
-  resources :stargazings
+  resources :astrophotographers, :constellations, :stargazings
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #get '/astrophotographers', to: 'astrophotographers#index'
 
