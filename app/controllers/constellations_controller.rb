5369e605b6ac81d3042f0d567c3f366509a61e98
chica25 <patricia@patriciaco.com>
2020-10-04 14:27:34 -0400

    Include Constellation links on the views pages

--- app/controllers/constellations_controller.rb
+++ app/controllers/constellations_controller.rb
@@ -9,5 +9,21 @@ class ConstellationsController < ApplicationController
     end
 
     def new
+        @constellation = Constellation.new
+    end
+
+    def create
+        @constellation = Constellation.new(const_params)
+        if @constellation.save
+            redirect_to  constellation_path(@constellation)
+        else
+         render :new
+        end
+    end
+
+    private
+
+    def const_params
+        params.require(:constellation).permit(:name, :description, :light_years_away_from_earth, :url_image)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :astrophotographers
   resources :constellations
-  get '/constellations', to: 'constellations#index'
-  get '/constellations/:id', to: 'constellations#show'
-  get '/constellations/new', to: 'constellations#new', as: 'new_constellation'
+  # get '/constellations', to: 'constellations#index'
+  # get '/constellations/:id', to: 'constellations#show'
+  # get '/constellations/new', to: 'constellations#new'
   resources :stargazings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #get '/astrophotographers', to: 'astrophotographers#index'
