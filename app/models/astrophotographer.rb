bd1c3f442e38035c884f1a060f91fe6dc9853cce
chica25 <patricia@patriciaco.com>
2020-10-21 02:27:13 -0400

    Modify Astrophotographer validation message

--- app/models/astrophotographer.rb
+++ app/models/astrophotographer.rb
@@ -5,7 +5,8 @@ class Astrophotographer < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }  
     validates :username, length: {minimum: 4 }
     validates :password, length: { in: 5..9 }
-    
+    validates :bio, presence: true, length: { maxium: 100 }
+
     
     def self.from_omniauth(auth) #=> user that's found or user that's created
         find_or_create_by(username: auth[:info][:email]) do |user|
