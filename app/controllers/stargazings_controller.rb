a520ca881cd9762912bc632a03b59f33fa6663d3
chica25 <patricia@patriciaco.com>
2020-10-25 18:48:55 -0400

    Add image to welcome page, modify links, refactor code

--- app/controllers/stargazings_controller.rb
+++ app/controllers/stargazings_controller.rb
@@ -14,19 +14,18 @@ class StargazingsController < ApplicationController
     end
   end
 
-  def show
-    @stargazing = Stargazing.find_by_id(params[:id])
-  end
-
-  
      def index
          if params[:constellation_id] && @constellation = Constellation.find_by_id(params[:id])
-             @stargazings = Stargazing.find_by_id(params[:id])
+             @stargazings = @constellation.stargazings
         else
             #add flash error message
            @stargazings = Stargazing.all
         end
-    end
+   end
+
+  def show
+    @stargazing = Stargazing.find_by_id(params[:id])
+  end
 
    def edit
       @stargazing = Stargazing.find_by_id(params[:id])
@@ -34,8 +33,8 @@ class StargazingsController < ApplicationController
 
 
    def update
-    set_star#=>@constellation = Constellation.find_by_id(params[:id])
-    if @stargazing.update(cons_params)
+    set_star #=>@constellation = Constellation.find_by_id(params[:id])
+    if @stargazing.update(star_params)
     redirect_to stargazing_path(@stargazing)
     else
         render :edit
@@ -43,7 +42,7 @@ class StargazingsController < ApplicationController
 end
 
 def destroy
-    set_cons #=>@constellation = Constellation.find_by_id(params[:id])
+    set_star #=>@constellation = Constellation.find_by_id(params[:id])
     if @stargazing.destroy
         redirect_to stargazings_path
     else 
@@ -51,14 +50,15 @@ def destroy
     end
 end
 
-
-
     private
 
     def star_params
       params.require(:stargazing).permit(:location, :weather, :time, :constellation_id)
     end
-    
+
+    def set_star
+      @stargazing = Stargazing.find_by_id(params[:id])
+    end
 
 end
 
