cdc5b84afeba28c7b0c6557f6cb1b4153c116657
chica25 <patricia@patriciaco.com>
2020-10-26 01:27:34 -0400

    Modify Stargazing form partials

--- app/models/astrophotographer.rb
+++ app/models/astrophotographer.rb
@@ -3,12 +3,12 @@ class Astrophotographer < ApplicationRecord
     has_many :constellations, through: :stargazings
     has_secure_password
     validates :username, presence: true, uniqueness: { case_sensitive: false }
-    # validates :username, length: {minimum: 4 }
+    validates :username, length: {minimum: 4 }
     
     scope :alpha, -> { order(:username) }
 
 
-    def self.from_omniauth(auth) #=> user that's found or user that's created
+    def self.from_omniauth(auth) 
         find_or_create_by(username: auth[:info][:email]) do |user|
         user.password = SecureRandom.hex(15)
         end
--- app/models/constellation.rb
+++ app/models/constellation.rb
@@ -2,9 +2,7 @@ class Constellation < ApplicationRecord
     has_many :stargazings
     has_many :astrophotographers, through: :stargazings
     validates :constellation_name, presence: true
-  
-   #ActiveRecord query 2
-#    scope :alpha, -> { order(:constellation_name)}
+ 
     scope :sorted, -> { order(:constellation_name) }
 
 end
