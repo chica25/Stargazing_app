be56c86b22f0044913e4efe688f4ad8956c42307
chica25 <patricia@patriciaco.com>
2020-10-26 13:02:58 -0400

    Refactor CRUD and Views

--- app/controllers/stargazings_controller.rb
+++ app/controllers/stargazings_controller.rb
@@ -29,11 +29,11 @@ class StargazingsController < ApplicationController
   end
 
   def edit
-    set_cons
-    if @constellation.astrophotographer_id == current_user
+    set_star
+    if @stargazing.astrophotographer_id == current_user
       render :edit
     else
-      redirect_to stargazing_path(@stargazing)
+      redirect_to constellation_stargazing_path(@constellation)
   end
 end
 
@@ -47,13 +47,34 @@ end
     # end
 
   def update
-  @stargazing = Stargazing.find_by_id(params[:id])
-  if @stargazing.update(star_params)
-    redirect_to stargazing_path(@stargazing)
-  else
-      render :edit
+    @stargazing = Stargazing.find_by_id(params[:id])
+    if @stargazing.astrophotographer_id != current_user
+      @stargazing.update(star_params)
+      redirect_to stargazing_constellation_path(@constellation)
+    else
+        redirect_to constellation_stargazings_path(@constellation)
+    end
   end
-end
+    # def update
+    #     set_cons
+    #     if @constellation.astrophotographer_id != current_user
+    #          @constellation.update(cons_params)
+    #         # flash.now[:error] = "You're not authorized"
+    #         redirect_to constellations_path(@constellation)
+    #     else
+    #         render :edit
+    #     end
+    # end
+
+    def destroy
+        @constellation = Constellation.find_by_id(params[:id])
+        if @stargazing.astrophotographer_id == current_user
+            @stargazing.destroy 
+        else 
+          # flash.now[:error] = "Please try again"
+            redirect_to constellation_stargazings_path
+        end
+    end
 
 # def destroy
 #     set_star #=>@constellation = Constellation.find_by_id(params[:id])
